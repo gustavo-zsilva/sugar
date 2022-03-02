24c49db96533230baf79de4513a0e3466801e719
Gustavo Zonta da Silva <gustavozontadasilva@gmail.com>
2022-03-02 00:41:43 -0300

    Finished Mobile UI

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head"
 import { Header } from "@components/Header"
 
+import { Button } from "@components/Button"
+import { Footer } from "@components/Footer"
 import { Container } from "../styles/pages/Home"
 
 export default function Home() {
@@ -12,6 +14,22 @@ export default function Home() {
         
         <div>
             <Header />
+            <main>
+                <div>
+                    <h1>Sweets for all tastes.</h1>
+                    <span>choose, click and enjoy.</span>
+                </div>
+                <div>
+                    <Button type="purple">
+                        <span>Browse Sweets</span>
+                    </Button>
+                    <span>or</span>
+                    <Button type="ghost">
+                        <span>Create an account</span>
+                    </Button>
+                </div>
+            </main>
+            <Footer />
         </div>
     </Container>
   )
--- src/styles/pages/Home.ts
+++ src/styles/pages/Home.ts
@@ -3,15 +3,51 @@ import { styled } from "../stitches.config";
 export const Container = styled('div', {
     maxWidth: '80rem',
     margin: 'auto',
+    height: '100vh',
+
+    '@sm': {
+        margin: '0 1rem',
+    },
 
     'div': {
         display: 'grid',
         gridTemplateAreas: `
             'Header Header'
-            'Main Hero'
+            'Main Main'
             'Footer Footer'
         `,
         gridTemplateColumns: '1fr 1fr',
-        gridTemplateRows: '1fr auto 1fr',
+        gridTemplateRows: 'auto 1fr auto',
+        height: '100%',
+        
+
+        'main': {
+            gridArea: 'Main',
+            display: 'flex',
+            flexDirection: 'column',
+            
+            'div': {
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+                textAlign: 'center',
+
+                'h1': {
+                    fontSize: '2.6rem',
+                    marginBottom: '1.2rem',
+                },
+                '> span': {
+                    fontSize: '1.125rem',
+                    color: '$secondary',
+                }
+            },
+            
+            'div:last-child': {
+                '> span': {
+                    transform: 'translateY(30%)'
+                }
+            }
+        }
     }
 })
+
